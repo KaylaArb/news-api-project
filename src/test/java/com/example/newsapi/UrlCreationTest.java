bfc49ce7388a8eb4c23f30ec7929aa76dd9e82a5
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2020-11-04 22:33:22 +0100

    recommit of unit test

--- src/test/java/com/example/newsapi/UrlCreationTest.java
+++ src/test/java/com/example/newsapi/UrlCreationTest.java
@@ -1,4 +1,25 @@
 package com.example.newsapi;
 
+import com.example.newsapi.newsapi.NewsAPIService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 public class UrlCreationTest {
+
+    NewsAPIService newsAPIService;
+
+    @Autowired
+    public void setNewsAPIService(NewsAPIService newsAPIService) { this.newsAPIService = newsAPIService; }
+
+    @Test
+    public void checkURLTest() {
+        String expectedURL= "https://newsapi.org/v2/top-headlines?country=pl&category=business&apiKey=ee409c1f26a54613ac37ac156c1df2c1";
+        assertEquals(expectedURL, newsAPIService.buildURL("pl", "business"), "Must return " + expectedURL);
+    }
+
+
 }
